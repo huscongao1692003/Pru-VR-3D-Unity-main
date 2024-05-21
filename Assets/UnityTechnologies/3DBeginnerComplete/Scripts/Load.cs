95e19f3aef05ea9d0afea86cdfb81e409f640489
Thanh Nguyen <72685335+huscongao1692003@users.noreply.github.com>
2024-05-21 18:52:59 +0700

    update, still glitch check range

--- Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Load.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Load.cs
@@ -8,8 +8,7 @@ public class Load : MonoBehaviour
    
     void Start()
     {
-        SceneManager.LoadSceneAsync("Gameplay");
-        //Additive mode adds the Scene to the current loaded Scenes, in this case Gameplay scene
+      
         SceneManager.LoadSceneAsync("Level00", LoadSceneMode.Additive);
     }
 
